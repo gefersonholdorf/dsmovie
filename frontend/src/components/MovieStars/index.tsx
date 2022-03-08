e1a21009a20b5935a1850f1238543dee590ecd75
gefersonholdorf <gefersonholdorf88@gmail.com>
2022-03-08 16:26:10 -0300

    MovieCard

---
+++ frontend/src/components/MovieStars/index.tsx
@@ -0,0 +1,19 @@
+import {ReactComponent as StarFull} from 'assents/img/star-full.svg';
+import {ReactComponent as StarHalf} from 'assents/img/star-half.svg';
+import {ReactComponent as StarEmpty} from 'assents/img/star-empty.svg';
+import './styles.css'
+
+function MovieStars() {
+
+    return (
+        <div className="dsmovie-stars-container">
+        <StarFull />
+        <StarFull />
+        <StarFull />
+        <StarHalf />
+        <StarEmpty />
+        </div>
+    );
+}
+
+export default MovieStars;
